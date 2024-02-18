867c8c0f895d0606b837213d5c503042c14ea7da
Samsonov366 <thelem@hotmail.fr>
2024-02-19 00:21:29 +0100

    more songs + added movie field in data

--- src/data/songs.js
+++ src/data/songs.js
@@ -1,14 +1,64 @@
-
-
 export const songs = [
   {
     title: "Summer",
     artist: "Joe Hisaishi",
-    filename: "https://www.dropbox.com/scl/fi/1sg0jjdjjbru5asurzfk1/summer.mp3?rlkey=jjpc70iur18gfjtt1a0s8nrir&raw=1",
+    movie: "Kikujiro's summer",
+    filename:
+      "https://www.dropbox.com/scl/fi/1sg0jjdjjbru5asurzfk1/summer.mp3?rlkey=jjpc70iur18gfjtt1a0s8nrir&raw=1",
   },
   {
     title: "Nara",
     artist: "Sofiane Pamart",
-    filename: "https://www.dropbox.com/scl/fi/2ppvatkgxg3227kuybr6y/nara.mp3?rlkey=lzbaohs43peikbsj81my2qhdf&raw=1",
+    filename:
+      "https://www.dropbox.com/scl/fi/2ppvatkgxg3227kuybr6y/nara.mp3?rlkey=lzbaohs43peikbsj81my2qhdf&raw=1",
+  },
+  {
+    title: "One summer day",
+    artist: "Joe Hisaishi",
+    movie: "Chihiro",
+    filename:
+      "https://www.dropbox.com/scl/fi/30ed24i9w3nqwjt87e3dk/one-summer-day.mp3?rlkey=zv16tygp7tnii3gvzijq2vi7u&raw=1",
+  },
+  {
+    title: "Waltz of Chihiro",
+    artist: "Joe Hisaishi",
+    movie: "Chihiro",
+    filename:
+      "https://www.dropbox.com/scl/fi/b0ocx40jt0ijxrim7f5j4/waltz-of-chihiro.mp3?rlkey=tplu8yp1ge2xvm59k2n73i2x7&raw=1",
+  },
+  {
+    title: "Howl's moving castle",
+    artist: "Joe Hisaishi",
+    movie: "Howl's moving castle",
+    filename:
+      "https://www.dropbox.com/scl/fi/4rtxou6dtnzophb5gfxiz/howls-moving-castle.mp3?rlkey=ufrqu2r90cy8wjf0n2i7eejqa&raw=1",
+  },  
+  {
+    title: "The legend of Ashitaka",
+    artist: "Joe Hisaishi",
+    movie: "Princess Mononoke",
+    filename:
+      "https://www.dropbox.com/scl/fi/jseqad2ccooglcnw0okse/mononoke.mp3?rlkey=ydau02dbeduu5lzrhi1a2g70e&raw=1",
+  },
+  {
+    title: "Sparkle",
+    artist: "RADWIMPS",
+    movie: "Kimi no Na wa",
+    filename:
+      "https://www.dropbox.com/scl/fi/n3bfx21xwkxrmqnty5r5f/kimi-no-na-wa.mp3?rlkey=ugh60xqg7tr2r4nr91vmeauou&raw=1",
+  },
+  {
+    title: "Clair de lune",
+    artist: "Claude Debussy",
+    movie: "",
+    filename:
+      "https://www.dropbox.com/scl/fi/4a6ucqloaq9cl22yjblov/clair-de-lune.mp3?rlkey=d6m9a5crrzo5wo66t2wgiyrps&raw=1",
+  },
+  {
+    title: "River flows in your",
+    artist: "Yiruma",
+    movie: "",
+    filename:
+      "https://www.dropbox.com/scl/fi/qr9f8qul8xdri6882cmuf/river-flows-in-your.mp3?rlkey=pewu2cmgjns7fapj4jvzp5ppv&raw=1",
   },
 ];
